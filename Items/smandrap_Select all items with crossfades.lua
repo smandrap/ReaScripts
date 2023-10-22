1a54b8bc1d7a07632b38161bd654f5bc52a72d4e
smandrap <60984250+smandrap@users.noreply.github.com>
2023-10-22 19:57:09 +0200

    Update smandrap_Select all items with crossfades.lua
    Support v7 Fixed Lanes

--- Items/smandrap_Select all items with crossfades.lua
+++ Items/smandrap_Select all items with crossfades.lua
@@ -1,18 +1,60 @@
 -- @description Select all items with crossfades
 -- @author smandrap
--- @version 0.1
+-- @donation https://paypal.me/smandrap
+-- @version 0.2
+-- @changelog
+--  + Support v7 FixedLanes
 -- @about
 --   Select all crossfaded items in project.
---   Will likely not work properly with fixed item lanes.
 
 local reaper = reaper
-
 local script_name = "smandrap_Select all items with crossfades"
+
+local reaper_version = tonumber(reaper.GetAppVersion():sub(1, 1))
+
 
 local overlapping_items = {}
 local crossfade_items = {}
 
-local function GetOverlappingItems()
+local function GetOverlappingItemsNew()
+  local tr_cnt = reaper.CountTracks(0)
+  if tr_cnt == 0 then return end
+  
+  local GetItemInfo = reaper.GetMediaItemInfo_Value
+  
+  for i = 0, tr_cnt - 1 do
+    local track = reaper.GetTrack(0, i)
+    local item_cnt = reaper.CountTrackMediaItems(track)
+    
+    local ordered_items = {}
+    
+    for j = 0, item_cnt - 1 do
+      local item = reaper.GetTrackMediaItem(track, j)
+      local fl = reaper.GetMediaItemInfo_Value(item, 'I_FIXEDLANE')
+      
+      if not ordered_items[fl] then ordered_items[fl] = {} end
+      
+      table.insert(ordered_items[fl], item)
+    end
+    
+    for _, fl_items in pairs(ordered_items) do
+      for j = 1, #fl_items do
+        if not fl_items[j + 1] then break end
+        
+        local item_end = GetItemInfo(fl_items[j], 'D_POSITION') + GetItemInfo(fl_items[j], 'D_LENGTH')
+        local next_item_start = GetItemInfo(fl_items[j + 1], 'D_POSITION')
+        
+        if item_end > next_item_start then
+          table.insert(overlapping_items, {fl_items[j], fl_items[j + 1]})
+        end
+      end
+    end
+    
+    ::continue::
+  end
+end
+
+local function GetOverlappingItemsOld()
   local tr_cnt = reaper.CountTracks(0)
   if tr_cnt == 0 then return end
   
@@ -51,7 +93,11 @@ end
 
 
 local function main()
-  GetOverlappingItems()
+  if reaper_version < 7 then 
+    GetOverlappingItemsOld()
+  else 
+    GetOverlappingItemsNew()
+  end
   
   for _, items in pairs(overlapping_items) do
     if CheckCrossfade(items[1], items[2]) then 
