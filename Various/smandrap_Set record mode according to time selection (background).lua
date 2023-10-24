b5cf88ea8e85bbd4eaaffce378bae464b60e0c4c
smandrap <60984250+smandrap@users.noreply.github.com>
2023-10-24 19:18:55 +0200

    Update smandrap_Set record mode according to time selection (background).lua

--- Various/smandrap_Set record mode according to time selection (background).lua
+++ Various/smandrap_Set record mode according to time selection (background).lua
@@ -1,15 +1,19 @@
 -- @description Set record mode according to time selection (background)
 -- @author smandrap
--- @version 1.0
+-- @version 1.1
+-- @changelog
+--  # Change state only when not recording
 -- @donation https://paypal.me/smandrap
-
+-- @about
+--  mlprod request. https://forum.cockos.com/showthread.php?t=284064
+--  
+--  Auto change record mode to Time selection autopunch if there's a time selection
+--  Does not change record mode during record if time selection is changed
 
 local GetPlayState = reaper.GetPlayState
 local GetTimeRange = reaper.GetSet_LoopTimeRange
 local OnCommand = reaper.Main_OnCommand
 local defer = reaper.defer
-
-local prev_rec_state = 0
 
 local function IsTimeSel()
   local s, e = GetTimeRange(false, false, nil, nil, false)
@@ -17,15 +21,7 @@ local function IsTimeSel()
 end
 
 local function main()
-  
-  local rec_state = GetPlayState()
-  
-  if prev_rec_state ~= rec_state and rec_state == 5 then
-    OnCommand(IsTimeSel() and 40076 or 40252, 0)
-  end
-  
-  prev_rec_state = rec_state
-  
+  if GetPlayState() ~= 5 then OnCommand(IsTimeSel() and 40076 or 40252, 0) end
   defer(main)
 end
 
