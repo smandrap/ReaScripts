1cc384733f934e8dff872833454de7135d2557ec
smandrap <60984250+smandrap@users.noreply.github.com>
2023-11-02 11:29:41 +0100

    Update smandrap_Set record mode according to time selection (background).lua

--- Various/smandrap_Set record mode according to time selection (background).lua
+++ Various/smandrap_Set record mode according to time selection (background).lua
@@ -1,9 +1,8 @@
 -- @description Set record mode according to time selection (background)
 -- @author smandrap
--- @version 1.2
+-- @version 1.2.1
 -- @changelog
---  + Avoid adding to the recent actions list
---  + Slow down polling rate (change poll_rate to speedup/slowdown polling)
+--  # Prevent user error with poll rate
 -- @donation https://paypal.me/smandrap
 -- @about
 --  mlprod request. https://forum.cockos.com/showthread.php?t=284064 \n
@@ -11,6 +10,12 @@
 --  
 --  Auto change record mode to Time selection autopunch if there's a time selection\n
 --  Does not change record mode during record if time selection is changed\n
+
+-----------------------
+
+local poll_rate = 11  -- Decrease this value if you want the script to update faster
+
+-----------------------
 
 if not reaper.CF_GetSWSVersion then
   reaper.MB('Download at \n https://www.sws-extension.org/', 'SWS Required', 0)
@@ -22,8 +27,6 @@ local GetTimeRange = reaper.GetSet_LoopTimeRange
 local SetIntConfigVar = reaper.SNM_SetIntConfigVar
 local Refresh = reaper.ThemeLayout_RefreshAll
 local defer = reaper.defer
-
-local poll_rate = 11
 local call_cnt = 0
 
 local function IsTimeSel()
@@ -34,7 +37,7 @@ end
 local function main()
   call_cnt = call_cnt + 1
   
-  if call_cnt == poll_rate then
+  if call_cnt >= poll_rate then
     if GetPlayState() ~= 5 then 
       SetIntConfigVar('projrecmode', IsTimeSel() and 2 or 1)
       Refresh()
