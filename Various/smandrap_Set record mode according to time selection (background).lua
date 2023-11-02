47cb0dd4c048029ce81d9c5de4bd54dfcade4d6c
smandrap <60984250+smandrap@users.noreply.github.com>
2023-11-02 16:47:04 +0100

    Update smandrap_Set record mode according to time selection (background).lua

--- Various/smandrap_Set record mode according to time selection (background).lua
+++ Various/smandrap_Set record mode according to time selection (background).lua
@@ -1,51 +1,46 @@
 -- @description Set record mode according to time selection (background)
 -- @author smandrap
--- @version 1.2.1
+-- @version 1.2.2
 -- @changelog
---  # Prevent user error with poll rate
+--  # Revert to previous behavior
+--  # Temporarily remove SWS dependency until i find out how tf to do things
 -- @donation https://paypal.me/smandrap
 -- @about
 --  mlprod request. https://forum.cockos.com/showthread.php?t=284064 \n
---  REQUIRES SWS \n
---  
+--
 --  Auto change record mode to Time selection autopunch if there's a time selection\n
 --  Does not change record mode during record if time selection is changed\n
 
 -----------------------
 
-local poll_rate = 11  -- Decrease this value if you want the script to update faster
+local poll_rate = 11 -- Decrease this value if you want the script to update faster
 
 -----------------------
 
-if not reaper.CF_GetSWSVersion then
+--[[ if not reaper.CF_GetSWSVersion then
   reaper.MB('Download at \n https://www.sws-extension.org/', 'SWS Required', 0)
   return
 end
+ ]]
 
-local GetPlayState = reaper.GetPlayState
-local GetTimeRange = reaper.GetSet_LoopTimeRange
-local SetIntConfigVar = reaper.SNM_SetIntConfigVar
-local Refresh = reaper.ThemeLayout_RefreshAll
-local defer = reaper.defer
 local call_cnt = 0
 
 local function IsTimeSel()
-  local s, e = GetTimeRange(false, false, nil, nil, false)
+  local s, e = reaper.GetSet_LoopTimeRange(false, false, nil, nil, false)
   return s ~= e
 end
 
 local function main()
   call_cnt = call_cnt + 1
-  
+
   if call_cnt >= poll_rate then
-    if GetPlayState() ~= 5 then 
-      SetIntConfigVar('projrecmode', IsTimeSel() and 2 or 1)
-      Refresh()
-    end
     call_cnt = 0
+    if reaper.GetPlayState() ~= 5 then
+      reaper.Main_OnCommand(IsTimeSel() and 40076 or 40252, 0)
+    end
   end
-  
-  defer(main)
+
+  reaper.defer(main)
 end
 
-main()
+reaper.defer(main)
