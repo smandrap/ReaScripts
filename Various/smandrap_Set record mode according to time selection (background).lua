57ff5b9a9599270f42a2b733c95448f26d2d0b61
smandrap <60984250+smandrap@users.noreply.github.com>
2023-11-02 11:20:35 +0100

    Update smandrap_Set record mode according to time selection (background).lua

--- Various/smandrap_Set record mode according to time selection (background).lua
+++ Various/smandrap_Set record mode according to time selection (background).lua
@@ -1,19 +1,30 @@
 -- @description Set record mode according to time selection (background)
 -- @author smandrap
--- @version 1.1
+-- @version 1.2
 -- @changelog
---  # Change state only when not recording
+--  + Avoid adding to the recent actions list
+--  + Slow down polling rate (change poll_rate to speedup/slowdown polling)
 -- @donation https://paypal.me/smandrap
 -- @about
---  mlprod request. https://forum.cockos.com/showthread.php?t=284064
+--  mlprod request. https://forum.cockos.com/showthread.php?t=284064 \n
+--  REQUIRES SWS \n
 --  
---  Auto change record mode to Time selection autopunch if there's a time selection
---  Does not change record mode during record if time selection is changed
+--  Auto change record mode to Time selection autopunch if there's a time selection\n
+--  Does not change record mode during record if time selection is changed\n
+
+if not reaper.CF_GetSWSVersion then
+  reaper.MB('Download at \n https://www.sws-extension.org/', 'SWS Required', 0)
+  return
+end
 
 local GetPlayState = reaper.GetPlayState
 local GetTimeRange = reaper.GetSet_LoopTimeRange
-local OnCommand = reaper.Main_OnCommand
+local SetIntConfigVar = reaper.SNM_SetIntConfigVar
+local Refresh = reaper.ThemeLayout_RefreshAll
 local defer = reaper.defer
+
+local poll_rate = 11
+local call_cnt = 0
 
 local function IsTimeSel()
   local s, e = GetTimeRange(false, false, nil, nil, false)
@@ -21,7 +32,16 @@ local function IsTimeSel()
 end
 
 local function main()
-  if GetPlayState() ~= 5 then OnCommand(IsTimeSel() and 40076 or 40252, 0) end
+  call_cnt = call_cnt + 1
+  
+  if call_cnt == poll_rate then
+    if GetPlayState() ~= 5 then 
+      SetIntConfigVar('projrecmode', IsTimeSel() and 2 or 1)
+      Refresh()
+    end
+    call_cnt = 0
+  end
+  
   defer(main)
 end
 
