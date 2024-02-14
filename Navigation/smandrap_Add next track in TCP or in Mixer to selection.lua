b5e803e1bbd952b10383589cde98d7f16888d935
smandrap <60984250+smandrap@users.noreply.github.com>
2024-02-14 18:06:34 +0100

    Update smandrap_Add next track in TCP or in Mixer to selection.lua

--- Navigation/smandrap_Add next track in TCP or in Mixer to selection.lua
+++ Navigation/smandrap_Add next track in TCP or in Mixer to selection.lua
@@ -1,3 +1,10 @@
+-- @description smandrap_Add next track in TCP or in Mixer to selection.lua
+-- @author smandrap
+-- @noindex
+-- @about Add next visible track in TCP or Mixer to selection. 
+-- @readme_skip
+
+
 local mixer_hwnd = reaper.JS_Window_Find("Mixer", true)
 local focus = reaper.JS_Window_GetParent(reaper.JS_Window_GetFocus())
 
