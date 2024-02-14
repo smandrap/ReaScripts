eb57a4028f48e692c7f08e9b804de27b5c5e04a2
smandrap <60984250+smandrap@users.noreply.github.com>
2024-02-14 17:58:56 +0100

    Create Script: smandrap_Select next track in TCP or in Mixer.lua

---
+++ Script: smandrap_Select next track in TCP or in Mixer.lua
@@ -0,0 +1,33 @@
+local mixer_hwnd = reaper.JS_Window_Find("Mixer", true)
+local focus = reaper.JS_Window_GetParent(reaper.JS_Window_GetFocus())
+
+local function next_in_mcp()
+  local first_sel_t = reaper.GetSelectedTrack(-1, 0) or reaper.GetLastTouchedTrack()
+  local id = 0
+  
+  if first_sel_t then id = reaper.GetMediaTrackInfo_Value(first_sel_t, 'IP_TRACKNUMBER') end
+  
+  
+  local tr_cnt = reaper.CountTracks()
+  if id >= tr_cnt then return end
+  
+ 
+  
+  for i = id, tr_cnt - 1 do
+    local t = reaper.GetTrack(-1, i)
+    if reaper.IsTrackVisible(t, true) then
+      reaper.SetOnlyTrackSelected(t)
+      return t
+    end
+  end
+  
+end
+
+
+if focus == mixer_hwnd then
+  reaper.Undo_BeginBlock()
+  next_in_mcp()
+  reaper.Undo_EndBlock("Select Next Track in Mixer", 0)
+else
+  reaper.Main_OnCommand(40285, 0)
+end
