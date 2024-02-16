81acdd565fa8011c0df0dade5c8a13122a7cfccf
smandrap <60984250+smandrap@users.noreply.github.com>
2024-02-16 12:02:45 +0100

    Oh boy i'm dumb

--- Navigation/smandrap_Go To Next-Previous Track (context aware) pack.lua
+++ Navigation/smandrap_Go To Next-Previous Track (context aware) pack.lua
@@ -1,6 +1,8 @@
 -- @author smandrap
 -- @description Go To Next-Previous track (context aware) pack
--- @version 1.0.1
+-- @version 1.0.2
+-- @changelog
+--   * Oh boy i'm dumb.
 -- @about
 --   If Mixer has focus, go to next/previous track that is visible in the mixer (or add it to the selection).\n
 --   Requires JS_Reascript API
