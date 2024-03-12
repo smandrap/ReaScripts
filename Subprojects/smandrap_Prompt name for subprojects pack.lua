36c4e668022ec07a85dcdc22cf75b9cd0f768488
smandrap <60984250+smandrap@users.noreply.github.com>
2024-03-12 16:10:30 +0100

    Create smandrap_Prompt name for subprojects pack.lua

---
+++ Subprojects/smandrap_Prompt name for subprojects pack.lua
@@ -0,0 +1,12 @@
+-- @author smandrap
+-- @description Prompt name for subprojects (pack)
+-- @version 1.0
+-- @changelog
+--   Initial
+-- @about
+--   When creating a subproject, prompts for a name and optionally create it in a Subprojects folder
+-- @metapackage
+-- @provides
+--   [main] smandrap_Insert new subproject in Subproject folder (prompt for name)
+--   [main] smandrap_Move items to subproject, prompt for name (non-destructive glue).lua
+--   [main] smandrap_Move tracks to subprojects, prompt for name.lua
