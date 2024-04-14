9668e2accbb7b6766f4c4f68b76398a197c63a29
manup <mnp.federico@gmail.com>
2024-04-14 18:52:10 +0200

    Duplicate Tracks: add envelope logic (sexan/arkadata function)

--- Tracks/smandrap_Duplicate Tracks.lua
+++ Tracks/smandrap_Duplicate Tracks.lua
@@ -32,61 +32,75 @@ local groupAssign = true
 
 local insertLastSel = false
 
+-- Sexan/ArkaData function
+local function DeleteAllEnvelopes(track)
+  for i = reaper.CountTrackEnvelopes(track), 1, -1 do
+    local env = reaper.GetTrackEnvelope(track, i - 1)
+    reaper.SetCursorContext(2, env)
+    reaper.Main_OnCommand(40332, 0) -- select all points
+    reaper.Main_OnCommand(40333, 0) -- delete all points
+    reaper.Main_OnCommand(40065, 0) -- remove env
+  end
+end
+
 local function DoDuplicateStuff()
   reaper.Undo_BeginBlock()
   reaper.PreventUIRefresh(-1)
   for i = 1, dupenum do
     reaper.Main_OnCommand(40062, 0) --Dupe
-    if not otherLanes then reaper.Main_OnCommand(42691, 0) end --Track lanes: Delete lanes (including media items) that are not playing
-    
+
+    --Track lanes: Delete lanes (including media items) that are not playing
+    if not otherLanes then reaper.Main_OnCommand(42691, 0) end
+
     -- TODO: Active Lane
-    if not activeLane then 
+    if not activeLane then
       reaper.Main_OnCommand(42796, 0)
     end
-    
-    -- TODO: ENVELOPES
-    if not envelopes then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_REMOVE_ALLENVS"), 0) end
+
+    if not envelopes then 
+      for j = 0, reaper.CountSelectedTracks(0) - 1 do
+        local track = reaper.GetSelectedTrack(0, j)
+        DeleteAllEnvelopes(track) 
+      end
+    end
     if not fx then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_CLRFXCHAIN3"), 0) end
     if not sends then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS6"), 0) end
     if not receives then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS5"), 0) end
     if not groupAssign then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_REMOVE_TR_GRP"), 0) end
   end
   reaper.PreventUIRefresh(1)
-  reaper.Undo_EndBlock("Duplicate Tracks "..dupenum.." times", 0)
+  reaper.Undo_EndBlock("Duplicate Tracks " .. dupenum .. " times", 0)
 end
 
-local function DoDuplicateStuff()
+local function DeleteActiveLanes()
   local track = reaper.GetTrack(0, 0)
   if not reaper.ValidatePtr(track, "MediaTrack*") then return end
-  
+
   local numlanes = reaper.GetMediaTrackInfo_Value(track, "I_NUMFIXEDLANES")
-  
+
   for i = 0, numlanes - 1 do
-    local laneplays = reaper.GetMediaTrackInfo_Value(track, "C_LANEPLAYS:"..i)
-    
-    reaper.SetMediaTrackInfo_Value(track, "C_LANEPLAYS:"..i, laneplays == 0 and 2 or 0)
+    local laneplays = reaper.GetMediaTrackInfo_Value(track, "C_LANEPLAYS:" .. i)
+
+    reaper.SetMediaTrackInfo_Value(track, "C_LANEPLAYS:" .. i, laneplays == 0 and 2 or 0)
   end
-  
-  reaper.Main_OnCommand(42691, 0)
-  
+
+  reaper.Main_OnCommand(42691, 0) --Track lanes: Delete lanes (including media items) that are not playing
+
   numlanes = reaper.GetMediaTrackInfo_Value(track, "I_NUMFIXEDLANES")
   for i = 0, numlanes - 1 do
-    reaper.SetMediaTrackInfo_Value(track, "C_LANEPLAYS:"..i, 0)
+    reaper.SetMediaTrackInfo_Value(track, "C_LANEPLAYS:" .. i, 0)
   end
-  
-  
-  reaper.UpdateItemLanes(0)
 end
 
 -- GUI
 
 local ctx = reaper.ImGui_CreateContext(script_name)
 local visible, open
-local window_flags = reaper.ImGui_WindowFlags_NoCollapse() | 
-                     reaper.ImGui_WindowFlags_NoResize()   |
-                     reaper.ImGui_WindowFlags_AlwaysAutoResize()
+local window_flags = reaper.ImGui_WindowFlags_NoCollapse() |
+    reaper.ImGui_WindowFlags_NoResize()   |
+    reaper.ImGui_WindowFlags_AlwaysAutoResize()
 local btn_w = 80
-local first_frame = true             
+local first_frame = true
 local font = reaper.ImGui_CreateFont('sans-serif', 12)
 reaper.ImGui_Attach(ctx, font)
 
@@ -95,53 +109,53 @@ local function DrawWindow()
 
   reaper.ImGui_AlignTextToFramePadding(ctx)
   reaper.ImGui_Text(ctx, "Number of duplicates:")
-  
+
   reaper.ImGui_SameLine(ctx)
   reaper.ImGui_PushItemWidth(ctx, 100)
   if first_frame then reaper.ImGui_SetKeyboardFocusHere(ctx) end
   _, dupenum = reaper.ImGui_InputInt(ctx, '##dupenum', dupenum, 0)
   dupenum = dupenum < 1 and 1 or dupenum
-  
+
   reaper.ImGui_Dummy(ctx, 0, 10)
   reaper.ImGui_Separator(ctx)
   reaper.ImGui_Dummy(ctx, 0, 10)
-  
+
   reaper.ImGui_Text(ctx, "Data to duplicate")
-  
+
   reaper.ImGui_Indent(ctx)
-  
+
   --_, activeLane = reaper.ImGui_Checkbox(ctx, 'Active Lane', activeLane)
   _, otherLanes = reaper.ImGui_Checkbox(ctx, 'Non-Active Lanes', otherLanes)
-  --_, envelopes = reaper.ImGui_Checkbox(ctx, 'Envelopes', envelopes)
+  _, envelopes = reaper.ImGui_Checkbox(ctx, 'Envelopes', envelopes)
   _, fx = reaper.ImGui_Checkbox(ctx, 'FX', fx)
   --_, instruments = reaper.ImGui_Checkbox(ctx, 'Instruments', instruments)
   _, sends = reaper.ImGui_Checkbox(ctx, 'Sends', sends)
   _, receives = reaper.ImGui_Checkbox(ctx, 'Receives', receives)
   _, groupAssign = reaper.ImGui_Checkbox(ctx, 'Group Assignments', groupAssign)
-  
+
   reaper.ImGui_Unindent(ctx)
-  
+
   --reaper.ImGui_Dummy(ctx, 0, 20)
   --_, insertLastSel = reaper.ImGui_Checkbox(ctx, 'Insert after last selected track', insertLastSel)
-  
-  
+
+
   reaper.ImGui_Dummy(ctx, 0, 10)
   reaper.ImGui_Separator(ctx)
   reaper.ImGui_Dummy(ctx, 0, 2)
-  
+
   reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w * 2 - 15)
-  
+
   reaper.ImGui_PushStyleColor(ctx, reaper.ImGui_Col_Button(), 0x5D5D5DAA)
   if reaper.ImGui_Button(ctx, "Cancel", btn_w) then open = false end
   reaper.ImGui_PopStyleColor(ctx)
   reaper.ImGui_SameLine(ctx)
-  
+
   reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w - 10)
   if reaper.ImGui_IsKeyPressed(ctx, reaper.ImGui_Key_Enter()) or reaper.ImGui_Button(ctx, "OK", btn_w) then
     DoDuplicateStuff()
     open = false
   end
-  
+
   reaper.ImGui_PopStyleVar(ctx) -- Frame rounding
 end
 
@@ -149,12 +163,12 @@ local function guiloop()
   reaper.ImGui_SetNextWindowSize(ctx, 240, 330, reaper.ImGui_Cond_FirstUseEver())
   reaper.ImGui_PushStyleVar(ctx, reaper.ImGui_StyleVar_WindowRounding(), 5)
   reaper.ImGui_PushFont(ctx, font)
-  
+
   visible, open = reaper.ImGui_Begin(ctx, script_name, true, window_flags)
   reaper.ImGui_PopStyleVar(ctx)
-  
+
   if reaper.ImGui_IsKeyPressed(ctx, reaper.ImGui_Key_Escape()) then open = false end
-  
+
   if visible then
     DrawWindow()
     reaper.ImGui_End(ctx)
