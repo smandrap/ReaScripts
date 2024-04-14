bbb2ea0959b5c9ce2402e8095bc75583f50fe533
smandrap <60984250+smandrap@users.noreply.github.com>
2024-04-15 00:22:09 +0200

    Update smandrap_Duplicate Tracks.lua

--- Tracks/smandrap_Duplicate Tracks.lua
+++ Tracks/smandrap_Duplicate Tracks.lua
@@ -1,8 +1,10 @@
 -- @description Duplicate Tracks
 -- @author smandrap
--- @version 1.0.2
+-- @version 1.0.3
 -- @changelog
---    # Set duplicated tracks as selected after duplication
+--    + Alt+click checkbox sets/unsets all checkboxes
+--    # Move Receives checkbox alongside Sends checkbox
+--    # Add SWS dependency check
 -- @donation https://paypal.me/smandrap
 -- @about
 --   Pro Tools style Duplicate Tracks window
@@ -11,8 +13,14 @@ local reaper = reaper
 local script_name = "Duplicate Tracks"
 
 if not reaper.ImGui_GetVersion() then
-  local ok = reaper.MB('Install now?', 'ReaImGui Missing', 1)
+  local ok = reaper.MB('Install now?\n\n(REAPER restart is required after install)', 'ReaImGui Missing', 1)
   if ok == 1 then reaper.ReaPack_BrowsePackages("ReaImGui API") end
+  return
+end
+
+if not reaper.CF_GetSWSVersion then
+  local ok = reaper.MB('Install now?\n\n(REAPER restart is required after install)', 'SWS Extensions Missing', 1)
+  if ok == 1 then reaper.ReaPack_BrowsePackages("SWS Extensions") end
   return
 end
 
@@ -23,14 +31,16 @@ reaper.set_action_options(3)
 local _ = nil
 local dupenum = 1
 
-local activeLane = true
-local otherLanes = true
-local envelopes = true
-local fx = true
-local instruments = true
-local sends = true
-local receives = true
-local groupAssign = true
+local dupeElements = {}
+
+dupeElements.activeLane = true
+dupeElements.otherLanes = true
+dupeElements.envelopes = true
+dupeElements.fx = true
+dupeElements.instruments = true
+dupeElements.sends = true
+dupeElements.receives = true
+dupeElements.groupAssign = true
 
 local duplicated_tracks = {}
 
@@ -85,21 +95,20 @@ local function DoDuplicateStuff()
 
     -- NON-ACTIVE LANES
     --Track lanes: Delete lanes (including media items) that are not playing
-    if not otherLanes then reaper.Main_OnCommand(42691, 0) end
+    if not dupeElements.otherLanes then reaper.Main_OnCommand(42691, 0) end
 
     -- ENVELOPES
-    if not envelopes then
+    if not dupeElements.envelopes then
       reaper.Main_OnCommand(41148, 0) -- Show All Envelopes on track
       for j = 1, #sel_tracks do
         DeleteAllEnvelopes(sel_tracks[j])
       end
     end
 
-
-    if not fx then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_CLRFXCHAIN3"), 0) end
-    if not sends then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS6"), 0) end
-    if not receives then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS5"), 0) end
-    if not groupAssign then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_REMOVE_TR_GRP"), 0) end
+    if not dupeElements.fx then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_CLRFXCHAIN3"), 0) end
+    if not dupeElements.sends then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS6"), 0) end
+    if not dupeElements.receives then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SENDS5"), 0) end
+    if not dupeElements.groupAssign then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_REMOVE_TR_GRP"), 0) end
 
     for j = 1, #sel_tracks do
       duplicated_tracks[#duplicated_tracks + 1] = sel_tracks[j]
@@ -115,8 +124,6 @@ local function DoDuplicateStuff()
   reaper.Undo_EndBlock("Duplicate Tracks " .. dupenum .. " times", 0)
 end
 
-
-
 -- GUI
 
 local ctx = reaper.ImGui_CreateContext(script_name)
@@ -128,6 +135,49 @@ local btn_w = 80
 local first_frame = true
 local font = reaper.ImGui_CreateFont('sans-serif', 12)
 reaper.ImGui_Attach(ctx, font)
+
+local function Checkbox(label, val)
+  local _, rv = reaper.ImGui_Checkbox(ctx, label, val)
+
+  if reaper.ImGui_IsKeyDown(ctx, reaper.ImGui_Key_LeftAlt()) and reaper.ImGui_IsItemEdited(ctx) then
+    for k, v in pairs(dupeElements) do
+      dupeElements[k] = rv
+    end
+  end
+
+  return rv
+end
+
+local function DrawCheckboxes()
+  --dupeElements.activeLane = Checkbox('Active Lane', dupeElements.activeLane)
+  dupeElements.otherLanes = Checkbox('Non-Active Lanes', dupeElements.otherLanes)
+  dupeElements.envelopes = Checkbox('Envelopes', dupeElements.envelopes)
+  dupeElements.fx = Checkbox('FX', dupeElements.fx)
+  --dupeElements.instruments = Checkbox('Instruments', dupeElements.instruments)
+  dupeElements.sends = Checkbox('Sends', dupeElements.sends)
+  reaper.ImGui_SameLine(ctx)
+  dupeElements.receives = Checkbox('Receives', dupeElements.receives)
+  dupeElements.groupAssign = Checkbox('Group Assignments', dupeElements.groupAssign)
+end
+
+local function DrawOkCancelButtons()
+  reaper.ImGui_Dummy(ctx, 0, 10)
+  reaper.ImGui_Separator(ctx)
+  reaper.ImGui_Dummy(ctx, 0, 2)
+
+  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w * 2 - 15)
+
+  reaper.ImGui_PushStyleColor(ctx, reaper.ImGui_Col_Button(), 0x5D5D5DAA)
+  if reaper.ImGui_Button(ctx, "Cancel", btn_w) then open = false end
+  reaper.ImGui_PopStyleColor(ctx)
+  reaper.ImGui_SameLine(ctx)
+
+  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w - 10)
+  if reaper.ImGui_Button(ctx, "OK", btn_w) then
+    DoDuplicateStuff()
+    open = false
+  end
+end
 
 local function DrawWindow()
   reaper.ImGui_PushStyleVar(ctx, reaper.ImGui_StyleVar_FrameRounding(), 5)
@@ -145,41 +195,14 @@ local function DrawWindow()
   reaper.ImGui_Separator(ctx)
   reaper.ImGui_Dummy(ctx, 0, 10)
 
-  reaper.ImGui_Text(ctx, "Data to duplicate")
-
-  reaper.ImGui_Indent(ctx)
-
-  --_, activeLane = reaper.ImGui_Checkbox(ctx, 'Active Lane', activeLane)
-  _, otherLanes = reaper.ImGui_Checkbox(ctx, 'Non-Active Lanes', otherLanes)
-  _, envelopes = reaper.ImGui_Checkbox(ctx, 'Envelopes', envelopes)
-  _, fx = reaper.ImGui_Checkbox(ctx, 'FX', fx)
-  --_, instruments = reaper.ImGui_Checkbox(ctx, 'Instruments', instruments)
-  _, sends = reaper.ImGui_Checkbox(ctx, 'Sends', sends)
-  _, receives = reaper.ImGui_Checkbox(ctx, 'Receives', receives)
-  _, groupAssign = reaper.ImGui_Checkbox(ctx, 'Group Assignments', groupAssign)
-
-  reaper.ImGui_Unindent(ctx)
-
-  --reaper.ImGui_Dummy(ctx, 0, 20)
-  --_, insertLastSel = reaper.ImGui_Checkbox(ctx, 'Insert after last selected track', insertLastSel)
-
-
-  reaper.ImGui_Dummy(ctx, 0, 10)
-  reaper.ImGui_Separator(ctx)
+  reaper.ImGui_Text(ctx, "Data to duplicate:")
   reaper.ImGui_Dummy(ctx, 0, 2)
 
-  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w * 2 - 15)
+  reaper.ImGui_Indent(ctx)
+  DrawCheckboxes()
+  reaper.ImGui_Unindent(ctx)
 
-  reaper.ImGui_PushStyleColor(ctx, reaper.ImGui_Col_Button(), 0x5D5D5DAA)
-  if reaper.ImGui_Button(ctx, "Cancel", btn_w) then open = false end
-  reaper.ImGui_PopStyleColor(ctx)
-  reaper.ImGui_SameLine(ctx)
-
-  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w - 10)
-  if reaper.ImGui_IsKeyPressed(ctx, reaper.ImGui_Key_Enter()) or reaper.ImGui_Button(ctx, "OK", btn_w) then
-    DoDuplicateStuff()
-    open = false
-  end
+  DrawOkCancelButtons()
 
   reaper.ImGui_PopStyleVar(ctx) -- Frame rounding
 end
@@ -193,7 +216,10 @@ local function guiloop()
   reaper.ImGui_PopStyleVar(ctx)
 
   if reaper.ImGui_IsKeyPressed(ctx, reaper.ImGui_Key_Escape()) then open = false end
-
+  if reaper.ImGui_IsKeyPressed(ctx, reaper.ImGui_Key_Enter()) then
+    DoDuplicateStuff()
+    open = false
+  end
   if visible then
     DrawWindow()
     reaper.ImGui_End(ctx)
