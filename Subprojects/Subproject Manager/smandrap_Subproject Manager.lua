a8a1471af97bd74f2c541eb5ae20f1a13751d6a5
manup <mnp.federico@gmail.com>
2024-04-26 14:20:50 +0200

    Make first gui

--- Subprojects/Subproject Manager/smandrap_Subproject Manager.lua
+++ Subprojects/Subproject Manager/smandrap_Subproject Manager.lua
@@ -19,16 +19,20 @@ end
 
 -- APP
 
+local _ = nil
 local os_sep = package.config:sub(1, 1)
 local rec_path = reaper.GetProjectPath()
 
-local subproject_path = ''
+local subproject_path = rec_path
+
+local use_template = false
+
+local template_folder = table.concat({ reaper.GetResourcePath(), os_sep, 'ProjectTemplates' })
 local template_path = ''
 
 local function InsertSubprojecFromTemplate()
   --local fileok, fn = reaper.GetUserFileNameForRead('ciao', 'Test', 'Test')
 
-  local template_folder = table.concat({ reaper.GetResourcePath(), os_sep, 'ProjectTemplates' })
   local ok, template = reaper.JS_Dialog_BrowseForOpenFiles('Template', template_folder, '', '.RPP', false)
   if not ok then return end
 
@@ -51,21 +55,65 @@ local function InsertSubprojecFromTemplate()
   reaper.Main_OnCommand(41816, 0) -- open associated project in tab
 end
 
+local function main()
+  InsertSubprojecFromTemplate()
+end
+
 
 -- GUI
 
 local ctx = reaper.ImGui_CreateContext(script_name)
 local visible, open
-local window_flags = reaper.ImGui_WindowFlags_None()
+local window_flags = reaper.ImGui_WindowFlags_None() | reaper.ImGui_WindowFlags_AlwaysAutoResize()
 local font = reaper.ImGui_CreateFont('sans-serif', 12)
+
+local btn_w = 80
+
 reaper.ImGui_Attach(ctx, font)
 
-local function DrawWindow()
-  
+local function DrawOkCancelButtons()
+  reaper.ImGui_Dummy(ctx, 0, 10)
+  reaper.ImGui_Separator(ctx)
+  reaper.ImGui_Dummy(ctx, 0, 2)
 
-  if reaper.ImGui_Button(ctx, 'DOIT') then
-    InsertSubprojecFromTemplate()
+  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w * 2 - 15)
+
+
+  reaper.ImGui_PushStyleColor(ctx, reaper.ImGui_Col_Button(), 0x5D5D5DAA)
+  if reaper.ImGui_Button(ctx, "Cancel", btn_w) then open = false end
+  reaper.ImGui_PopStyleColor(ctx)
+  reaper.ImGui_SameLine(ctx)
+
+  reaper.ImGui_SetCursorPosX(ctx, reaper.ImGui_GetWindowWidth(ctx) - btn_w - 10)
+  if reaper.ImGui_Button(ctx, "OK", btn_w) then
+    main()
+    open = false
   end
+end
+
+local function DrawWindow()
+  reaper.ImGui_Text(ctx, 'Path :')
+  reaper.ImGui_PushItemWidth(ctx, 400)
+  _, subproject_path = reaper.ImGui_InputText(ctx, '##txtin_subprojFn', subproject_path)
+  reaper.ImGui_SameLine(ctx, nil, 0)
+  if reaper.ImGui_Button(ctx, '...##btn_pathselect') then
+    local ok, temp_path = reaper.JS_Dialog_BrowseForFolder('Select Location', subproject_path)
+    if ok == 1 then subproject_path = temp_path end
+  end
+
+  reaper.ImGui_Dummy(ctx, 0, 10)
+  _, use_template = reaper.ImGui_Checkbox(ctx, 'Create from Project Template', use_template)
+  if use_template then
+    reaper.ImGui_PushItemWidth(ctx, 400)
+    _, template_path = reaper.ImGui_InputText(ctx, '##txtin_templateFn', template_path)
+    reaper.ImGui_SameLine(ctx, nil, 0)
+    if reaper.ImGui_Button(ctx, '...##btn_templatepathselect') then
+      local ok, temp_path = reaper.JS_Dialog_BrowseForOpenFiles('Select Template', template_folder, '','.rpp', false)
+      if ok then template_path = temp_path end
+    end
+  end
+
+  DrawOkCancelButtons()
 end
 
 local function guiloop()
