5cf8e6eb1d8b952026bb71df1ae580d40e9d0b48
manup <mnp.federico@gmail.com>
2024-04-27 19:50:03 +0200

    Change tempo envelope timebase to time during the operation

--- Tempo/smandrap_Process Bars.lua
+++ Tempo/smandrap_Process Bars.lua
@@ -1,7 +1,7 @@
 local reaper = reaper
 
 --TODO: GUI
---TODO: Restore state of "ignore project tempo" on items 
+--TODO: Restore state of "ignore project tempo" on items
 --TODO: Scale all tempo markers in between the target bars
 --TODO: implement stevie insert/delete bars
 --TODO: Preserve tempo at end of changes
@@ -16,7 +16,7 @@ local target_timesig_denom = 4
 local tempos = {}
 
 for i = start_bar, (start_bar + length_in_bars) - 1 do
-  local _, _, _, timesig_nom, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
+  local _, _, _, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
   tempos[i] = {
     timesig_num = timesig_num,
     timesig_denom = timesig_denom,
@@ -33,35 +33,44 @@ for i = 1, reaper.CountMediaItems(0) do
   if reaper.IsMediaItemSelected(item) then sel_items[#sel_items + 1] = item end
   if reaper.TakeIsMIDI(reaper.GetActiveTake(item)) then midi_items[#midi_items + 1] = item end
 end
+-- BEGIN
+local timebase = reaper.SNM_GetIntConfigVar('tempoenvtimelock', -1)
+reaper.SNM_SetIntConfigVar('tempoenvtimelock', 0)
+
+reaper.Undo_BeginBlock()
 
 reaper.Main_OnCommand(40289, 0) --unselect all items
 
 for i = 1, #midi_items do
-  reaper.SetMediaItemSelected(midi_items[i], true)  
+  reaper.SetMediaItemSelected(midi_items[i], true)
 end
 
 reaper.Main_OnCommand(43094, 0) -- Set midi item timebase to time, ignore project tempo
 
 
-reaper.Undo_BeginBlock()
 
 for i = start_bar, start_bar + length_in_bars - 1 do
   local _, qn_start, qn_end, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
-  
+
   if timesig_num ~= target_timesig_num then
     local time_start = reaper.TimeMap_QNToTime(qn_start)
     local tgt_tempo = tempos[i].tempo * (target_timesig_num / target_timesig_denom) / (timesig_num / timesig_denom)
-    
-    reaper.SetTempoTimeSigMarker(0, -1, -1, i, 0, tgt_tempo, i == start_bar and target_timesig_num or 0, i == start_bar and target_timesig_denom or 0, false)
+
+    reaper.SetTempoTimeSigMarker(0, -1, -1, i, 0, tgt_tempo, i == start_bar and target_timesig_num or 0,
+      i == start_bar and target_timesig_denom or 0, false)
   end
 end
+
+reaper.SetTempoTimeSigMarker(0, -1, -1, start_bar + length_in_bars, 0, tempos[#tempos].tempo, tempos[#tempos]
+.timesig_num, tempos[#tempos].timesig_denom, false)
+
 
 reaper.Main_OnCommand(40289, 0) --unselect all items
 
 for i = 1, #sel_items do
-  reaper.SetMediaItemSelected(sel_items[i], true)  
+  reaper.SetMediaItemSelected(sel_items[i], true)
 end
 
-
+reaper.SNM_SetIntConfigVar('tempoenvtimelock', timebase)
 reaper.Undo_EndBlock("doit", -1)
 reaper.UpdateTimeline()
