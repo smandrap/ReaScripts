835576755a9b65765046fce9ef684482b7bcd60f
manup <mnp.federico@gmail.com>
2024-04-27 20:38:20 +0200

    fix some things, SEEMS to work now

--- Tempo/smandrap_Process Bars.lua
+++ Tempo/smandrap_Process Bars.lua
@@ -8,8 +8,9 @@ local reaper = reaper
 --TODO: Replace bars
 
 
+
 local start_bar = 1
-local length_in_bars = 2
+local length_in_bars = 7
 local target_timesig_num = 3
 local target_timesig_denom = 4
 
@@ -45,33 +46,39 @@ for i = 1, #midi_items do
   reaper.SetMediaItemSelected(midi_items[i], true)
 end
 
-reaper.Main_OnCommand(43094, 0) -- Set midi item timebase to time, ignore project tempo
+reaper.Main_OnCommand(43096, 0) -- Set midi item timebase to beats, ignore project tempo
 
 local tempochange_cnt = reaper.CountTempoTimeSigMarkers(0)
+
+local add_final_tempomarker = false
 
 for i = start_bar, start_bar + length_in_bars - 1 do
   local _, qn_start, qn_end, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
 
   if timesig_num ~= target_timesig_num then
+    add_final_tempomarker = true
     local time_start = reaper.TimeMap_QNToTime(qn_start)
     local tgt_tempo = tempos[i].tempo * (target_timesig_num / target_timesig_denom) / (timesig_num / timesig_denom)
 
     reaper.SetTempoTimeSigMarker(0, -1, -1, i, 0, tgt_tempo, i == start_bar and target_timesig_num or 0,
       i == start_bar and target_timesig_denom or 0, false)
-  end
 
-  for j = 0, tempochange_cnt - 1 do
-    local _, tpos, measpos, beatpos, bpm, ts_num, ts_denom, lin = reaper.GetTempoTimeSigMarker(0, j)
-    if measpos == i and tpos > reaper.TimeMap_QNToTime(qn_start) and tpos < reaper.TimeMap_QNToTime(qn_end) then
-      local scaled_tempo = bpm * (target_timesig_num / target_timesig_denom) / (ts_num / ts_denom)
-      reaper.SetTempoTimeSigMarker(0, j, tpos, -1, -1, scaled_tempo, -1, -1, lin)
+    -- Scale all tempo markers in this measure
+    for j = 0, tempochange_cnt - 1 do
+      local _, tpos, measpos, beatpos, bpm, ts_num, ts_denom, lin = reaper.GetTempoTimeSigMarker(0, j)
+      if measpos == i and tpos > reaper.TimeMap_QNToTime(qn_start) and tpos < reaper.TimeMap_QNToTime(qn_end) then
+        local scaled_tempo = bpm * (target_timesig_num / target_timesig_denom) / (ts_num / ts_denom)
+        reaper.SetTempoTimeSigMarker(0, j, tpos, -1, -1, scaled_tempo, -1, -1, lin)
+      end
     end
   end
-
 end
 
+-- Restore whatever is needed at end
+if add_final_tempomarker then
 reaper.SetTempoTimeSigMarker(0, -1, -1, start_bar + length_in_bars, 0, tempos[#tempos].tempo, tempos[#tempos]
-.timesig_num, tempos[#tempos].timesig_denom, false)
+  .timesig_num, tempos[#tempos].timesig_denom, false)
+end
 
 
 reaper.Main_OnCommand(40289, 0) --unselect all items
