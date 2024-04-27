978d26764cf130c2e0b10a7e0e96169b6794213e
manup <mnp.federico@gmail.com>
2024-04-27 20:10:54 +0200

    scale intermediate tempo markers

--- Tempo/smandrap_Process Bars.lua
+++ Tempo/smandrap_Process Bars.lua
@@ -47,7 +47,7 @@ end
 
 reaper.Main_OnCommand(43094, 0) -- Set midi item timebase to time, ignore project tempo
 
-
+local tempochange_cnt = reaper.CountTempoTimeSigMarkers(0)
 
 for i = start_bar, start_bar + length_in_bars - 1 do
   local _, qn_start, qn_end, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
@@ -59,6 +59,15 @@ for i = start_bar, start_bar + length_in_bars - 1 do
     reaper.SetTempoTimeSigMarker(0, -1, -1, i, 0, tgt_tempo, i == start_bar and target_timesig_num or 0,
       i == start_bar and target_timesig_denom or 0, false)
   end
+
+  for j = 0, tempochange_cnt - 1 do
+    local _, tpos, measpos, beatpos, bpm, ts_num, ts_denom, lin = reaper.GetTempoTimeSigMarker(0, j)
+    if measpos == i and tpos > reaper.TimeMap_QNToTime(qn_start) and tpos < reaper.TimeMap_QNToTime(qn_end) then
+      local scaled_tempo = bpm * (target_timesig_num / target_timesig_denom) / (ts_num / ts_denom)
+      reaper.SetTempoTimeSigMarker(0, j, tpos, -1, -1, scaled_tempo, -1, -1, lin)
+    end
+  end
+
 end
 
 reaper.SetTempoTimeSigMarker(0, -1, -1, start_bar + length_in_bars, 0, tempos[#tempos].tempo, tempos[#tempos]
