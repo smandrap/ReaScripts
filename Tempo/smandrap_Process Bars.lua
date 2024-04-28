7aa772e88bcbba0384ed0dc4e4ef94e9463646d4
manup <mnp.federico@gmail.com>
2024-04-28 17:58:44 +0200

    change some var names

--- Tempo/smandrap_Process Bars.lua
+++ Tempo/smandrap_Process Bars.lua
@@ -1,25 +1,30 @@
-local reaper = reaper
-
 --TODO: GUI
---FIXME: Restore state of "ignore project tempo" on items that precedently had it set
 --TODO: implement stevie insert/delete bars
 --TODO: Replace bars
 --TODO: account for project measure offset in the calculation
-
---TODO: Preserve tempo at end of changes
---TODO: Scale all tempo markers in between the target bars (done)
+--TODO: Refactor ReinterpretBars function into smaller functions
 
 
+--FIXME: Restore state of "ignore project tempo" on items that precedently had it set
 --FIXME: measures are zero based, account for this
+--FIXME: override audio items timebase to time?
+--FIXME: break if tpos > sec_end (doesn't belong to the current measure)
+--FIXME: preserve linear tempo changes at start of measures
+
+
+local reaper = reaper
+
 local start_bar = 1
 local length_in_bars = 3
 local target_timesig_num = 3
 local target_timesig_denom = 4
 
 local function ReinterpretBars()
+  local end_bar = start_bar + length_in_bars
+
   -- Cache measure information
   local tempos = {}
-  for i = start_bar, (start_bar + length_in_bars) - 1 do
+  for i = start_bar, end_bar - 1 do
     local _, _, _, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
     tempos[i] = {
       timesig_num = timesig_num,
@@ -27,6 +32,12 @@ local function ReinterpretBars()
       tempo = tempo
     }
   end
+
+  -- OVERRIDE TIMEBASES
+  local timebase_tempoenv = reaper.SNM_GetIntConfigVar('tempoenvtimelock', -1)
+  local timebase_itemmarkers = reaper.SNM_GetIntConfigVar('tempoenvtimelock', -1)
+  reaper.SNM_SetIntConfigVar('tempoenvtimelock', 0)
+  reaper.SNM_SetIntConfigVar('itemtimelock', 0)
 
   local sel_items = {}
   local midi_items = {}
@@ -37,26 +48,23 @@ local function ReinterpretBars()
     if reaper.IsMediaItemSelected(item) then sel_items[#sel_items + 1] = item end
     if reaper.TakeIsMIDI(reaper.GetActiveTake(item)) then midi_items[#midi_items + 1] = item end
   end
-  local timebase_tempoenv = reaper.SNM_GetIntConfigVar('tempoenvtimelock', -1)
-  local timebase_itemmarkers = reaper.SNM_GetIntConfigVar('tempoenvtimelock', -1)
-  reaper.SNM_SetIntConfigVar('tempoenvtimelock', 0)
-  reaper.SNM_SetIntConfigVar('itemtimelock', 0)
-  
+
+
   reaper.Main_OnCommand(40289, 0) --unselect all items
-  
+
   for i = 1, #midi_items do
     reaper.SetMediaItemSelected(midi_items[i], true)
   end
-  
+
   reaper.Main_OnCommand(43096, 0) -- Set midi item timebase to beats, ignore project tempo
-  
+
   -- BEGIN
   local tempochange_cnt = reaper.CountTempoTimeSigMarkers(0)
 
   -- If time is not reinterpreted (bars are already of the target time signature) there's no need to add tempo markers
   local add_final_tempomarker = false
 
-  for i = start_bar, start_bar + length_in_bars - 1 do
+  for i = start_bar, end_bar - 1 do
     local _, qn_start, qn_end, timesig_num, timesig_denom, tempo = reaper.TimeMap_GetMeasureInfo(0, i)
 
     if timesig_num ~= target_timesig_num then
@@ -64,12 +72,12 @@ local function ReinterpretBars()
       local sec_start = reaper.TimeMap_QNToTime(qn_start)
       local sec_end = reaper.TimeMap_QNToTime(qn_start)
       local target_tempo = tempos[i].tempo * (target_timesig_num / target_timesig_denom) / (timesig_num / timesig_denom)
-      
+
       --FIXME: preserve linear tempo changes
-        --local _, _, _, _, _, _, _, is_linear = reaper.GetTempoTimeSigMarker(0, j)
+      --local _, _, _, _, _, _, _, is_linear = reaper.GetTempoTimeSigMarker(0, j)
 
       reaper.SetTempoTimeSigMarker(0, -1, -1, i, 0, target_tempo, i == start_bar and target_timesig_num or 0,
-        i == start_bar and target_timesig_denom or 0, is_linear)
+        i == start_bar and target_timesig_denom or 0, false)
 
       -- Scale all tempo markers in this measure
       for j = 0, tempochange_cnt - 1 do
@@ -87,17 +95,18 @@ local function ReinterpretBars()
 
   -- Restore whatever is needed at end, but only if some stuff were altered
   if add_final_tempomarker then
-    reaper.SetTempoTimeSigMarker(0, -1, -1, start_bar + length_in_bars, 0, tempos[#tempos].tempo, tempos[#tempos]
-      .timesig_num, tempos[#tempos].timesig_denom, false)
+    local last_tmrk = tempos[#tempos]
+    reaper.SetTempoTimeSigMarker(0, -1, -1, end_bar, 0, last_tmrk.tempo, last_tmrk.timesig_num,
+      last_tmrk.timesig_denom, false)
   end
 
+  -- RESTORE TIMEBASE AND SELECTIONS
   reaper.Main_OnCommand(40289, 0) --unselect all items
 
   for i = 1, #sel_items do
     reaper.SetMediaItemSelected(sel_items[i], true)
   end
 
-  -- Restore timebases
   reaper.SNM_SetIntConfigVar('tempoenvtimelock', timebase_tempoenv)
   reaper.SNM_SetIntConfigVar('itemtimelock', timebase_itemmarkers)
 
