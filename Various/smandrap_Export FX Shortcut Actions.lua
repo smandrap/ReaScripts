97778029bc21e70f07aab8519ed856f00be026c3
manup <mnp.federico@gmail.com>
2024-04-30 13:26:58 +0200

    searchbox

--- Various/smandrap_Export FX Shortcut Actions.lua
+++ Various/smandrap_Export FX Shortcut Actions.lua
@@ -20,24 +20,38 @@ if not ImGui.GetVersion then
   return
 end
 
+local function table_copy(t)
+  local t2 = {}
+  for k, v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
+local function table_delete(t)
+  for i = 0, #t do t[i] = nil end
+end
+
 -- APP
 
 local FX_LIST = nil
 local FILTERED_FXLIST = nil
 
+local filter = ''
+
 local function GetFXList()
   local rv = true
   local i = 0
   local s = nil
-  
+
   local fx_list = {}
-  
-  while rv  do
+
+  while rv do
     rv, s = r.EnumInstalledFX(i)
     if rv then fx_list[#fx_list + 1] = s end
     i = i + 1
   end
-  
+
   return fx_list
 end
 
@@ -59,21 +73,37 @@ local font = ImGui.CreateFont('sans-serif', settings.font_size)
 ImGui.Attach(ctx, font)
 
 local function DrawSearchFilter()
-  
+  local change = false
+  change, filter = ImGui.InputText(ctx, '##DrawSearchFilter', filter)
+  return change
 end
 
 local function DrawFXList()
   ImGui.BeginListBox(ctx, '##fxlist', ImGui.GetWindowWidth(ctx) - 40, ImGui.GetWindowHeight(ctx) - 60)
-    for i = 1, #FX_LIST do
-      ImGui.Checkbox(ctx, '##fx'..i, false)
-      ImGui.SameLine(ctx)
-      ImGui.Text(ctx, FX_LIST[i])
-    end
+  for i = 1, #FILTERED_FXLIST do
+    if FILTERED_FXLIST[i] == nil then goto continue end
+    ImGui.Checkbox(ctx, '##fx' .. i, false)
+    ImGui.SameLine(ctx)
+    ImGui.Text(ctx, FILTERED_FXLIST[i])
+    ::continue::
+  end
   ImGui.EndListBox(ctx)
 end
 
+local function UpdateList()
+  if filter == '' then 
+    FILTERED_FXLIST = table_copy(FX_LIST)
+    return
+  end
+  table_delete(FILTERED_FXLIST)
+  for i = 1, #FX_LIST do
+    if FX_LIST[i]:lower():match(filter:lower()) then table.insert(FILTERED_FXLIST, FX_LIST[i]) end
+  end
+end
+
 local function DrawWindow()
-  DrawSearchFiletr()
+  local changed = DrawSearchFilter()
+  if changed then UpdateList() end
   DrawFXList()
 end
 
@@ -101,7 +131,7 @@ end
 
 local function init()
   FX_LIST = GetFXList()
-  FILTERED_FXLIST = FX_LIST
+  FILTERED_FXLIST = table_copy(FX_LIST)
 end
 
 local function Exit()
