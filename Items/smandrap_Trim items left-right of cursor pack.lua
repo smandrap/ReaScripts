5c24650c0471dafd5ce673f757847b4fd9df0b90
smandrap <60984250+smandrap@users.noreply.github.com>
2024-11-23 16:07:39 +0100

    Update smandrap_Trim items left-right of cursor pack.lua

--- Items/smandrap_Trim items left-right of cursor pack.lua
+++ Items/smandrap_Trim items left-right of cursor pack.lua
@@ -1,6 +1,6 @@
 -- @author smandrap
 -- @description Trim items left-right of cursor pack
--- @version 1.1
+-- @version 1.2
 -- @changelog
 --   - Fix script crashing at exit in certain situations (grouped items, etc)
 -- @about
